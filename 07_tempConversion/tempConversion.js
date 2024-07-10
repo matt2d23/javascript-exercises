6426b1ee377f8e0a4b05d1870b1e13054ce623e4
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-10 18:36:57 -0500

    Completion of exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(initial) {
+  let math = ((initial-32) * 5/9);
+  if (math % 1 === 0) {
+    return math;
+  } else {
+    return parseFloat(math.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(initial) {
+  let math = ((9/5)* initial) + 32;
+  if (math % 1 === 0){
+    return math;
+  } else {
+    return parseFloat(math.toFixed(1));
+  }
 };
 
 // Do not edit below this line
