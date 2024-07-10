d7dedcbcb8f12a8b3043db5140a7bedbd2278cc7
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-10 14:06:02 -0500

    Completion of exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let result = '';
+    for (let i = 1; i <= input.length; i++) {
+        result += input.substr(-i, 1);
+    }
+    return result;
+    
 };
 
 // Do not edit below this line
