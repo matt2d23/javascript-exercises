6306e7c5e87f7a885173c142358ee357794c79f6
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-10 16:16:20 -0500

    Completion of exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+
+let result = [];
+
+arr.forEach((item) => {
+    if (!toRemove.includes(item)) {
+        result.push(item);
+    }   
+});
+
+return result;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
