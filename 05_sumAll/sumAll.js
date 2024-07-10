95e76d81ba865115517f09029f852d02d9a6db16
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-10 18:04:37 -0500

    Completion of exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let final = 0;
 
+    let sortArgs = Array.from(arguments).sort((a,b) => a-b);
+    
+    min = sortArgs[0];
+    max = sortArgs[1];
+
+    if ((min < 0 || max < 0) || (typeof(min) !== 'number' || typeof(max) !== 'number')) {
+        return 'ERROR';
+    } else {
+        for (let i = min; i <= max; i++) {
+            final += i
+        }
+        return final;
+    };
+    
 };
 
 // Do not edit below this line
