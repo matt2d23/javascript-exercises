d89fb55afa9e906a0e4e33dc2c83e7c66d978ec2
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-13 01:52:24 -0500

    Completion of exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arr = [0,1];
+    let i;
+    
+    for (i = 2; i <= num; i++) {
+        arr[i] = arr[i-2] + arr[i-1];
+    }
 
+    return num < 0 ? 'OOPS' : 
+            num == 0 ? 0 : 
+            num == 1 ? 1 : arr[arr.length-1];    
 };
 
 // Do not edit below this line
