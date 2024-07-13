11a9e763b7c725fe14d31dfe3570e15f59d89cd8
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-13 01:34:13 -0500

    Completion of exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const validChars = "abcdefghijklmnopqrstuvwxyz123456789";
 
+    const cleanString = str.toLowerCase()
+            .split('')
+            .filter((item) => validChars.includes(item))
+            .join('');
+
+    const reversedString = cleanString.split('').reverse().join('');
+
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
